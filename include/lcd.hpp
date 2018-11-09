0a8bad55ecc5b93c9ccf44673ece4b32af571a57
Maurits van Altvorst <mvanaltvorst@icloud.com>
2018-11-09 19:01:07 +0100

    Create component files

---
+++ include/lcd.hpp
@@ -0,0 +1,8 @@
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "I2Cbus.hpp"
+
+class LCD {
+    LCD();
+
+}
---
+++ lcd.cpp
@@ -0,0 +1,4 @@
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "I2Cbus.hpp"
+
